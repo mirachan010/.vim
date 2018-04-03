dabb464c4476eca1da133fa183f42b3b8fd52006
mirachan010 <mirachan010@gmail.com>
2018-04-03 21:46:37 +0900

    .vimrcのいどうにつき削除

--- ubuntu.sh
+++
@@ -1,2 +0,0 @@
-ln -s ~/.vim/configs/users/.vimrc ~/.vimrc
-ln -s ~/.vim/configs/users/_viminfo ~/_viminfo
--- win.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
- mklink %HOMEDRIVE%%HOMEPATH%"\.vimrc" %HOMEDRIVE%%HOMEPATH%"\.vim\configs\users\.vimrc"
- mklink %HOMEDRIVE%%HOMEPATH%"\_viminfo" %HOMEDRIVE%%HOMEPATH%"\.vim\configs\users\_viminfo"
